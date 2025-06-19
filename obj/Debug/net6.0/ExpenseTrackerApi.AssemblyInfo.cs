fdc5bed67d6b25d2def16bb486a378c9be266ed5
vishal1009 <66193423+vishal1009@users.noreply.github.com>
2025-06-20 03:17:14 +0530

    Clean up for Railway deploy

--- obj/Debug/net6.0/ExpenseTrackerApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/ExpenseTrackerApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f4028242e6c1f89073531c2b235aef6fe05c9ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca7a2b555e6605dceecd3d76ed8f345ec401901")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
